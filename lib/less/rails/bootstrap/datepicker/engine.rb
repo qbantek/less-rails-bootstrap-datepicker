8cb83afdaa4d8b180220917259eb1826b392c6bf
Erich Quintero <qbantek@gmail.com>
2015-07-16 12:49:37 -0400

    framework no longer needed

--- lib/less/rails/bootstrap/datepicker/engine.rb
+++ lib/less/rails/bootstrap/datepicker/engine.rb
@@ -6,7 +6,7 @@ module Less
       module Datepicker
         class Engine < ::Rails::Engine
           initializer 'less-rails-bootstrap-datepicker.setup', :after => 'less-rails.before.load_config_initializers', :group => :all do |app|
-            app.config.less.paths << File.join(config.root, 'app', 'frameworks')
+            app.config.less.paths << File.join(config.root, 'app')
           end
         end
       end
